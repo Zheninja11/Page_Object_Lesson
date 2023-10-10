fd850a3913e5c9f62222a634b01dee14e743e659
Zheninja11 <strukov0504@inbox.ru>
2023-10-10 09:23:55 +0300

    Добавлен локатор для добавления в корзину

--- pages/locators.py
+++ pages/locators.py
@@ -7,3 +7,6 @@ class MainPageLocators():
 class LoginPageLocators():
     REGISTER_FORM = (By.XPATH, "//form[@id='register_form']")
     LOGIN_FORM = (By.XPATH, "//form[@id='login_form']")
+    
+class ProductPageLocators():
+    ADD_TO_BASKET_BUTTON = (By.XPATH, "//button[@class='btn btn-lg btn-primary btn-add-to-basket']")
