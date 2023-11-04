f071f50679160a11cb5ffab755308e35b79869b7
Zheninja11 <strukov0504@inbox.ru>
2023-11-04 14:18:15 +0300

    Добавил новые тесты

--- test_product_page.py
+++ test_product_page.py
@@ -1,13 +1,50 @@
-from .pages.main_page import MainPage
 from .pages.product_page import PageObject
+import pytest
 import time
 
+
 def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer"
     page = PageObject(browser, link, timeout=5)
     page.open()
     page.add_to_basket()
-    time.sleep(2)
     page.solve_quiz_and_get_code()
-    
-    
+    page.should_be_correct_name()
+    page.should_be_correct_price()
+
+@pytest.mark.xfail
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer"
+    page = PageObject(browser, link, timeout=5)
+    page.open()
+    page.add_to_basket()
+    page.solve_quiz_and_get_code()
+    page.should_not_be_success_message()
+
+def test_guest_cant_see_success_message(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer"
+    page = PageObject(browser, link, timeout=5)
+    page.open()
+    page.should_not_be_success_message()
+
+@pytest.mark.xfail
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer"
+    page = PageObject(browser, link, timeout=5)
+    page.open()
+    page.add_to_basket()
+    page.solve_quiz_and_get_code()
+    page.should_disappeared()
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer"
+    page = PageObject(browser, link, timeout=5)
+    page.open()
+    page.should_be_login_link()
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer"
+    page = PageObject(browser, link, timeout=5)
+    page.open()
+    page.go_to_login_page()
+    time.sleep(5)
