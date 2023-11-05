62457323b791588dbb01f4d784daa061b94a17c8
Zheninja11 <strukov0504@inbox.ru>
2023-11-05 22:57:40 +0300

    Добавил метод для формы регистрации

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,5 +1,6 @@
 from .base_page import BasePage
-from .locators import MainPageLocators
+from .locators import LoginPageLocators
+import time
 
 
 class LoginPage(BasePage):
@@ -13,9 +14,17 @@ class LoginPage(BasePage):
         assert True
 
     def should_be_login_form(self):
-        self.is_element_present(*MainPageLocators.LOGIN_FORM), "Login form input is not presented"
+        self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form input is not presented"
         assert True
 
     def should_be_register_form(self):
-        self.is_element_present(*MainPageLocators.REGISTER_FORM), "Registration form input is not presented"
+        self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Registration form input is not presented"
         assert True
+    
+    def  register_new_user(self, email = str(time.time()) + "@fakemail.org", password = 'test1234'):
+        email = str(time.time()) + "@fakemail.org"
+        password = '123456789Nn'
+        self.browser.find_element(*LoginPageLocators.REGISTER_INPUT_EMAIL).send_keys(email)
+        self.browser.find_element(*LoginPageLocators.REGISTER_INPUT_PASSWORD).send_keys(password)
+        self.browser.find_element(*LoginPageLocators.REGISTER_INPUT_PASSWORD_CONFIRM).send_keys(password)
+        self.browser.find_element(*LoginPageLocators.REGISTER_BUTTON_REGISTER).click()
