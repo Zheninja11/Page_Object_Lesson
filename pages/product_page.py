3b57598fd9257644f195c30ec1c05eb97ac3e8f5
Zheninja11 <strukov0504@inbox.ru>
2023-11-05 22:58:53 +0300

    Добавлены методы о добавлении товара в корзину

--- pages/product_page.py
+++ pages/product_page.py
@@ -6,3 +6,35 @@ class PageObject(BasePage):
     def add_to_basket(self):
         link = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON)
         link.click()
+    
+    def should_be_correct_price(self):
+        product_price_page = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE_PAGE)
+        product_price_value = product_price_page.text
+
+        product_price_basket = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE_BASKET)
+        product_price_basket_value = product_price_basket.text[14:-12]  # Выбираю цену путем слайса строки
+
+        assert product_price_value == product_price_basket_value, \
+            f"Expeсted price: {product_price_value}. Actual price: {product_price_basket_value}."
+
+        print("Correct price")
+
+    def should_be_correct_name(self):
+        product_name_page = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME_PAGE)
+        product_name_value = product_name_page.text
+
+        product_name_basket = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME_BASKET)
+        product_name_basket_value = product_name_basket.text
+
+        assert product_name_value == product_name_basket_value, \
+            f"Expected name: {product_name_value}. Actual name: {product_name_basket_value}."
+
+        print("Correct name")
+    
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+       "Success message is presented, but should not be"
+    
+    def should_disappeared(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE),\
+        "Success message is not disappeared"
