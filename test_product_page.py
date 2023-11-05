ce8412b3e090e67957b5edea72fa6ff12460a323
Zheninja11 <strukov0504@inbox.ru>
2023-11-05 23:02:10 +0300

    Добавил тестовый класс

--- test_product_page.py
+++ test_product_page.py
@@ -1,10 +1,13 @@
 from .pages.product_page import PageObject
+from .pages.login_page import LoginPage
 import pytest
 import time
 
 
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer"
+@pytest.mark.need_review
+@pytest.mark.parametrize('promo_offer', ["0","1", "3", "4", "5", "6", "7", "8", "9"])
+def test_guest_can_add_product_to_basket(browser, promo_offer):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo_offer}"
     page = PageObject(browser, link, timeout=5)
     page.open()
     page.add_to_basket()
@@ -42,9 +45,45 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.open()
     page.should_be_login_link()
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer"
     page = PageObject(browser, link, timeout=5)
     page.open()
     page.go_to_login_page()
     time.sleep(5)
+
+@pytest.mark.need_review
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer"
+    page = PageObject(browser, link,timeout=5)
+    page.open()
+    page.guest_clik_button_see_basket()
+    page.should_be_empty_basket()
+
+class TestUserAddToBasketFromProductPage():
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+        loginPage = LoginPage(browser, link)
+        loginPage.open()
+        loginPage.register_new_user()
+        time.sleep(10)
+        loginPage.should_be_authorized_user()
+    
+    
+    def test_user_cant_see_success_message(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer"
+        page = PageObject(browser, link, timeout=5)
+        page.open()
+        page.should_not_be_success_message()
+    
+    @pytest.mark.need_review
+    def test_user_can_add_product_to_basket(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer"
+        page = PageObject(browser, link, timeout=5)
+        page.open()
+        page.add_to_basket()
+        page.solve_quiz_and_get_code()
+        page.should_be_correct_name()
+        page.should_be_correct_price()
